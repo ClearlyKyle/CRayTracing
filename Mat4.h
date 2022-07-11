1c2afed1f2f30f261d42aed55a4ba95c2c228f45
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-07-11 21:34:18 +0100

    Scale and Inverse

--- Mat4.h
+++ Mat4.h
@@ -33,6 +33,26 @@ inline Mat4 Mat4_Transpose(const Mat4 m)
     return dest;
 }
 
+inline void Mat4_Scale(Mat4 *const m, const double s)
+{
+    m->elements[0][0] *= s;
+    m->elements[0][1] *= s;
+    m->elements[0][2] *= s;
+    m->elements[0][3] *= s;
+    m->elements[1][0] *= s;
+    m->elements[1][1] *= s;
+    m->elements[1][2] *= s;
+    m->elements[1][3] *= s;
+    m->elements[2][0] *= s;
+    m->elements[2][1] *= s;
+    m->elements[2][2] *= s;
+    m->elements[2][3] *= s;
+    m->elements[3][0] *= s;
+    m->elements[3][1] *= s;
+    m->elements[3][2] *= s;
+    m->elements[3][3] *= s;
+}
+
 inline Mat4 Mat4_Scale_Make(const vec3 v)
 {
     Mat4 dest = {0};
@@ -40,6 +60,7 @@ inline Mat4 Mat4_Scale_Make(const vec3 v)
     dest.elements[0][0] = v.x;
     dest.elements[1][1] = v.y;
     dest.elements[2][2] = v.z;
+    dest.elements[3][3] = 1.0;
 
     return dest;
 }
@@ -53,6 +74,9 @@ inline Mat4 Mat4_ZRot_Make(const double angle)
     dest.elements[1][0] = sin(angle);
     dest.elements[1][1] = cos(angle);
 
+    dest.elements[2][2] = 1.0;
+    dest.elements[3][3] = 1.0;
+
     return dest;
 }
 
@@ -65,6 +89,9 @@ inline Mat4 Mat4_YRot_Make(const double angle)
     dest.elements[2][0] = -sin(angle);
     dest.elements[2][2] = cos(angle);
 
+    dest.elements[1][1] = 1.0;
+    dest.elements[3][3] = 1.0;
+
     return dest;
 }
 
@@ -76,6 +103,9 @@ inline Mat4 Mat4_XRot_Make(const double angle)
     dest.elements[1][2] = -sin(angle);
     dest.elements[2][1] = sin(angle);
     dest.elements[2][2] = cos(angle);
+
+    dest.elements[0][0] = 1.0;
+    dest.elements[3][3] = 1.0;
 
     return dest;
 }
@@ -174,24 +204,88 @@ inline Mat4 Mat4_Mul_Mat4(const Mat4 m1, const Mat4 m2)
 
 inline Mat4 Mat4_Make_Transform(const vec3 v)
 {
-    Mat4 res = {0};
+    Mat4 dest = {0};
 
-    res.elements[0][3] = v.x;
-    res.elements[1][3] = v.y;
-    res.elements[2][3] = v.z;
+    dest.elements[0][3] = v.x;
+    dest.elements[1][3] = v.y;
+    dest.elements[2][3] = v.z;
 
-    return res;
+    dest.elements[0][0] = 1.0;
+    dest.elements[1][1] = 1.0;
+    dest.elements[2][2] = 1.0;
+    dest.elements[3][3] = 1.0;
+
+    return dest;
 }
 
 inline vec4 Mat4_mul_vec4(const Mat4 m, const vec4 v)
 {
-    vec4 res = {0};
-    res.x    = m.elements[0][0] * v.x + m.elements[1][0] * v.y + m.elements[2][0] * v.z + m.elements[3][0] * v.w;
-    res.y    = m.elements[0][1] * v.x + m.elements[1][1] * v.y + m.elements[2][1] * v.z + m.elements[3][1] * v.w;
-    res.z    = m.elements[0][2] * v.x + m.elements[1][2] * v.y + m.elements[2][2] * v.z + m.elements[3][2] * v.w;
-    res.w    = m.elements[0][3] * v.x + m.elements[1][3] * v.y + m.elements[2][3] * v.z + m.elements[3][3] * v.w;
+    vec4 dest = {0};
+    dest.x    = m.elements[0][0] * v.x + m.elements[1][0] * v.y + m.elements[2][0] * v.z + m.elements[3][0] * v.w;
+    dest.y    = m.elements[0][1] * v.x + m.elements[1][1] * v.y + m.elements[2][1] * v.z + m.elements[3][1] * v.w;
+    dest.z    = m.elements[0][2] * v.x + m.elements[1][2] * v.y + m.elements[2][2] * v.z + m.elements[3][2] * v.w;
+    dest.w    = m.elements[0][3] * v.x + m.elements[1][3] * v.y + m.elements[2][3] * v.z + m.elements[3][3] * v.w;
 
-    return res;
+    return dest;
+}
+
+inline Mat4 Mat4_Inverse(const Mat4 mat)
+{
+    Mat4 dest = {0};
+
+    double t[6];
+    double det;
+    double a = mat.elements[0][0], b = mat.elements[0][1], c = mat.elements[0][2], d = mat.elements[0][3],
+           e = mat.elements[1][0], f = mat.elements[1][1], g = mat.elements[1][2], h = mat.elements[1][3],
+           i = mat.elements[2][0], j = mat.elements[2][1], k = mat.elements[2][2], l = mat.elements[2][3],
+           m = mat.elements[3][0], n = mat.elements[3][1], o = mat.elements[3][2], p = mat.elements[3][3];
+
+    t[0] = k * p - o * l;
+    t[1] = j * p - n * l;
+    t[2] = j * o - n * k;
+    t[3] = i * p - m * l;
+    t[4] = i * o - m * k;
+    t[5] = i * n - m * j;
+
+    dest.elements[0][0] = f * t[0] - g * t[1] + h * t[2];
+    dest.elements[1][0] = -(e * t[0] - g * t[3] + h * t[4]);
+    dest.elements[2][0] = e * t[1] - f * t[3] + h * t[5];
+    dest.elements[3][0] = -(e * t[2] - f * t[4] + g * t[5]);
+
+    dest.elements[0][1] = -(b * t[0] - c * t[1] + d * t[2]);
+    dest.elements[1][1] = a * t[0] - c * t[3] + d * t[4];
+    dest.elements[2][1] = -(a * t[1] - b * t[3] + d * t[5]);
+    dest.elements[3][1] = a * t[2] - b * t[4] + c * t[5];
+
+    t[0] = g * p - o * h;
+    t[1] = f * p - n * h;
+    t[2] = f * o - n * g;
+    t[3] = e * p - m * h;
+    t[4] = e * o - m * g;
+    t[5] = e * n - m * f;
+
+    dest.elements[0][2] = b * t[0] - c * t[1] + d * t[2];
+    dest.elements[1][2] = -(a * t[0] - c * t[3] + d * t[4]);
+    dest.elements[2][2] = a * t[1] - b * t[3] + d * t[5];
+    dest.elements[3][2] = -(a * t[2] - b * t[4] + c * t[5]);
+
+    t[0] = g * l - k * h;
+    t[1] = f * l - j * h;
+    t[2] = f * k - j * g;
+    t[3] = e * l - i * h;
+    t[4] = e * k - i * g;
+    t[5] = e * j - i * f;
+
+    dest.elements[0][3] = -(b * t[0] - c * t[1] + d * t[2]);
+    dest.elements[1][3] = a * t[0] - c * t[3] + d * t[4];
+    dest.elements[2][3] = -(a * t[1] - b * t[3] + d * t[5]);
+    dest.elements[3][3] = a * t[2] - b * t[4] + c * t[5];
+
+    det = 1.0 / (a * dest.elements[0][0] + b * dest.elements[1][0] + c * dest.elements[2][0] + d * dest.elements[3][0]);
+
+    Mat4_Scale(&dest, det);
+
+    return dest;
 }
 
 #endif // __TRANSFORMS_H__
