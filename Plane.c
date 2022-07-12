5b1560dd03860847f36854106f34b5a5608b388b
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-07-12 22:16:25 +0100

    Plane test and transform added

--- Plane.c
+++ Plane.c
@@ -1,16 +1,18 @@
 #include "Plane.h"
 
-bool Plane_Test_Intersection(const Plane p, const Transform trans, const Ray_t *ray, vec3 *int_point, vec3 *local_normal, vec3 *local_colour)
+#define CLOSE_ENOUGH(D1, D2) fabs((D1) - (D2)) < 1e-21f
+
+bool Plane_Test_Intersection(const Plane plane, const Ray_t *ray, vec3 *int_point, vec3 *local_normal, vec3 *local_colour)
 {
     // Copy the ray and apply the backwards transform.
-    Ray_t back_ray = Transform_Apply_Ray(trans, *ray, false);
+    Ray_t back_ray = Transform_Apply_Ray(plane.transform, *ray, false);
 
     // Copy the m_lab vector from bckRay and normalize it.
     vec3 k = NORMALISE_VEC3(back_ray.lab);
 
     // Check if there is an intersection, ie. if the castRay is not parallel to the plane
     // if (!CloseEnough(k.GetElement(2), 0.0))
-    if (k.z == 0.0f)
+    if (!CLOSE_ENOUGH(k.z, 0.0))
     {
         // There is an intersection.
         const double t = back_ray.point1.z / -k.z;
@@ -29,18 +31,18 @@ bool Plane_Test_Intersection(const Plane p, const Transform trans, const Ray_t *
                 const vec3 poi = vec3_add(back_ray.point1, vec3_mul_scal(k, t));
 
                 // Transform the intersection point back into world coordinates.
-                *int_point = Transform_Apply_Forward(trans, poi);
+                *int_point = Transform_Apply_Forward(plane.transform, poi);
 
                 // Compute the local normal.
-                vec3 localOrigin  = {0.0f, 0.0f, 0.0f};
-                vec3 normalVector = {0.0f, 0.0f, -1.0f};
+                const vec3 localOrigin  = {0.0, 0.0, 0.0};
+                const vec3 normalVector = {0.0, 0.0, -1.0};
 
-                vec3 globalOrigin = Transform_Apply_Forward(trans, localOrigin);
-                *local_normal     = vec3_sub(Transform_Apply_Forward(trans, normalVector), globalOrigin);
-                *local_normal     = NORMALISE_VEC3(*local_normal);
+                const vec3 globalOrigin = Transform_Apply_Forward(plane.transform, localOrigin);
+                *local_normal           = vec3_sub(Transform_Apply_Forward(plane.transform, normalVector), globalOrigin);
+                *local_normal           = NORMALISE_VEC3(*local_normal);
 
                 // Return the base color.
-                *local_colour = p.colour;
+                *local_colour = plane.colour;
 
                 return true;
             }
--- Plane.h
+++ Plane.h
@@ -9,10 +9,10 @@
 
 typedef struct Plane
 {
-    vec3     colour;
+    vec3      colour;
     Transform transform;
 } Plane;
 
-bool Plane_Test_Intersection(const Plane p, const Transform trans, const Ray_t *ray, vec3 *int_point, vec3 *local_normal, vec3 *local_colour);
+bool Plane_Test_Intersection(const Plane plane, const Ray_t *ray, vec3 *int_point, vec3 *local_normal, vec3 *local_colour);
 
 #endif // __PLANE_H__
