99a3185b01c79323b74e7fdfc43b2e38f599f059
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-08-20 22:04:45 +0100

    mat3_mul_mat3

--- src/Maths/Mat3.h
+++ src/Maths/Mat3.h
@@ -19,4 +19,27 @@ inline vec3 mat3_mul_vec3(const mat3 mat, const vec3 vec)
     return out;
 }
 
+inline mat3 mat3_mul_mat3(const mat3 mat1, const mat3 mat2)
+{
+    mat3 dest = {0};
+
+    double a00 = mat1.elements[0][0], a01 = mat1.elements[0][1], a02 = mat1.elements[0][2],
+           a10 = mat1.elements[1][0], a11 = mat1.elements[1][1], a12 = mat1.elements[1][2],
+           a20 = mat1.elements[2][0], a21 = mat1.elements[2][1], a22 = mat1.elements[2][2],
+
+           b00 = mat2.elements[0][0], b01 = mat2.elements[0][1], b02 = mat2.elements[0][2],
+           b10 = mat2.elements[1][0], b11 = mat2.elements[1][1], b12 = mat2.elements[1][2],
+           b20 = mat2.elements[2][0], b21 = mat2.elements[2][1], b22 = mat2.elements[2][2];
+
+    dest.elements[0][0] = a00 * b00 + a10 * b01 + a20 * b02;
+    dest.elements[0][1] = a01 * b00 + a11 * b01 + a21 * b02;
+    dest.elements[0][2] = a02 * b00 + a12 * b01 + a22 * b02;
+    dest.elements[1][0] = a00 * b10 + a10 * b11 + a20 * b12;
+    dest.elements[1][1] = a01 * b10 + a11 * b11 + a21 * b12;
+    dest.elements[1][2] = a02 * b10 + a12 * b11 + a22 * b12;
+    dest.elements[2][0] = a00 * b20 + a10 * b21 + a20 * b22;
+    dest.elements[2][1] = a01 * b20 + a11 * b21 + a21 * b22;
+    dest.elements[2][2] = a02 * b20 + a12 * b21 + a22 * b22;
+}
+
 #endif // __MAT3_H__
