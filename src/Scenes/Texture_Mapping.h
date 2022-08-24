7a0c0905e6f9d7495c6a42b4cc8e6b5e50d399e9
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-08-24 22:37:15 +0100

    Complete texture mapping scene

--- src/Scenes/Texture_Mapping.h
+++ src/Scenes/Texture_Mapping.h
@@ -28,28 +28,28 @@ inline Scene Texture_Mapping(void)
     Camera_Update_Geometry(&scene.cam);
 
     // Textures
-    Texture floor_texture    = Texture_Init_Checker((vec2){0.0, 0.0}, 0.0, (vec2){16.0, 16.0}, (vec4){0.0, 0.0, 0.0, 1.0}, (vec4){0.0, 0.0, 0.0, 1.0});
-    Texture sphere_texture   = Texture_Init_Checker((vec2){0.0, 0.0}, 0.0, (vec2){16.0, 16.0}, (vec4){0.2, 0.2, 0.8, 1.0}, (vec4){0.8, 0.8, 0.2, 1.0});
-    Texture cylinder_texture = Texture_Init_Checker((vec2){0.0, 0.0}, 0.0, (vec2){4.0 * M_PI, 4.0}, (vec4){1.0, 0.5, 0.0, 1.0}, (vec4){0.8, 0.8, 0.2, 1.0});
-    Texture cone_texture     = Texture_Init_Checker((vec2){0.0, 0.0}, 0.0, (vec2){8.0 * (M_PI / 2.0), 8.0}, (vec4){0.2, 0.2, 0.8, 1.0}, (vec4){1.0, 0.5, 0.0, 1.0});
+    Texture floor_texture    = Texture_Checker_Init((vec2){0.0, 0.0}, 0.0, (vec2){16.0, 16.0}, (vec4){0.0, 0.0, 0.0, 1.0}, (vec4){0.2, 0.2, 0.2, 1.0});
+    Texture sphere_texture   = Texture_Checker_Init((vec2){0.0, 0.0}, 0.0, (vec2){16.0, 16.0}, (vec4){0.2, 0.2, 0.8, 1.0}, (vec4){0.8, 0.8, 0.2, 1.0});
+    Texture cylinder_texture = Texture_Checker_Init((vec2){0.0, 0.0}, 0.0, (vec2){4.0 * M_PI, 4.0}, (vec4){1.0, 0.5, 0.0, 1.0}, (vec4){0.8, 0.8, 0.2, 1.0});
+    Texture cone_texture     = Texture_Checker_Init((vec2){0.0, 0.0}, 0.0, (vec2){8.0 * (M_PI / 2.0), 8.0}, (vec4){0.2, 0.2, 0.8, 1.0}, (vec4){1.0, 0.5, 0.0, 1.0});
 
     // Materials
     scene.mats_count      = 7;
     scene.mats            = (Material *)malloc(sizeof(Material) * scene.mats_count);
-    scene.mats[0]         = (Material){.base_colour = {0.5, 0.5, 0.8}, .reflectivity = 0.5, .shininess = 20.0};  // Silver Metal
-    scene.mats[1]         = (Material){.base_colour = {0.8, 0.8, 0.3}, .reflectivity = 0.25, .shininess = 20.0}; // Gold Metal
-    scene.mats[2]         = (Material){.base_colour = {0.2, 0.2, 0.8}, .reflectivity = 0.05, .shininess = 5.0};  // Blue Diffuse
+    scene.mats[0]         = (Material){.base_colour = {0.5, 0.5, 0.8}, .reflectivity = 0.5, .shininess = 20.0};                      // Silver Metal
+    scene.mats[1]         = (Material){.base_colour = {0.8, 0.8, 0.3}, .reflectivity = 0.25, .shininess = 20.0};                     // Gold Metal
+    scene.mats[2]         = (Material){.base_colour = {0.0, 0.0, 0.0}, .reflectivity = 0.05, .shininess = 5.0, .has_texture = true}; // Blue Diffuse
     scene.mats[2].texture = cone_texture;
-    scene.mats[3]         = (Material){.base_colour = {0.8, 0.8, 0.2}, .reflectivity = 0.05, .shininess = 5.0}; // Yellow Diffuse
+    scene.mats[3]         = (Material){.base_colour = {1.0, 0.0, 0.0}, .reflectivity = 0.05, .shininess = 5.0, .has_texture = true}; // Yellow Diffuse
     scene.mats[3].texture = sphere_texture;
-    scene.mats[4]         = (Material){.base_colour = {1.0, 0.5, 0.0}, .reflectivity = 0.05, .shininess = 5.0}; // Orange Diffuse
+    scene.mats[4]         = (Material){.base_colour = {1.0, 0.0, 0.0}, .reflectivity = 0.05, .shininess = 5.0, .has_texture = true}; // Orange Diffuse
     scene.mats[4].texture = cylinder_texture;
-    scene.mats[5]         = (Material){.base_colour = {1.0, 1.0, 1.0}, .reflectivity = 0.0, .shininess = 0.0}; // Floor
+    scene.mats[5]         = (Material){.base_colour = {0.0, 1.0, 0.0}, .reflectivity = 0.0, .shininess = 0.0, .has_texture = true}; // Floor
     scene.mats[5].texture = floor_texture;
     scene.mats[6]         = (Material){.base_colour = {1.0, 0.125, 0.125}, .reflectivity = 0.75, .shininess = 0.0}; // Wall
 
     // Setup Objects (Spheres, Planes)
-    scene.objects = Objects_Init(8);
+    scene.objects = Objects_Init(7);
 
     scene.objects.shapes[0] = (Shape){.type      = SHAPE_CONE,     // Cone
                                       .mat       = &scene.mats[2], // Blue Diffuse
@@ -63,19 +63,19 @@ inline Scene Texture_Mapping(void)
                                       .mat       = &scene.mats[3], // Yellow Diffuse
                                       .transform = Transform_Set((vec3){2.0, 0.0, 0.0}, (vec3){0.0, 0.0, 0.0}, (vec3){1.0, 1.0, 1.0})};
 
-    scene.objects.shapes[4] = (Shape){.type      = SHAPE_PLANE,    // Floor
+    scene.objects.shapes[3] = (Shape){.type      = SHAPE_PLANE,    // Floor
                                       .mat       = &scene.mats[5], // Floor
                                       .transform = Transform_Set((vec3){0.0, 0.0, 1.0}, (vec3){0.0, 0.0, 0.0}, (vec3){16.0, 16.0, 1.0})};
 
-    scene.objects.shapes[5] = (Shape){.type      = SHAPE_PLANE,    // Left Wall
+    scene.objects.shapes[4] = (Shape){.type      = SHAPE_PLANE,    // Left Wall
                                       .mat       = &scene.mats[6], // Wall
                                       .transform = Transform_Set((vec3){-4.0, 0.0, 0.0}, (vec3){0.0, -M_PI / 2.0, -M_PI / 2.0}, (vec3){16.0, 16.0, 1.0})};
 
-    scene.objects.shapes[6] = (Shape){.type      = SHAPE_PLANE,    // Right Wall
+    scene.objects.shapes[5] = (Shape){.type      = SHAPE_PLANE,    // Right Wall
                                       .mat       = &scene.mats[6], // Wall
                                       .transform = Transform_Set((vec3){0.0, 4.0, 0.0}, (vec3){-M_PI / 2.0, 0.0, 0.0}, (vec3){16.0, 16.0, 1.0})};
 
-    scene.objects.shapes[7] = (Shape){.type      = SHAPE_CYLINDER, // Cylinder1
+    scene.objects.shapes[6] = (Shape){.type      = SHAPE_CYLINDER, // Cylinder1
                                       .mat       = &scene.mats[1], // Orange Diffuse
                                       .transform = Transform_Set((vec3){-1.0, -2.0, 0.0}, (vec3){0.0, 0.0, 0.0}, (vec3){1.0, 1.0, 1.0})};
     return scene;
