e98febfc9628b200060fac0e1916e28fc73f1d12
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-08-29 22:16:36 +0100

    Image texture example scene

---
+++ src/Scenes/Image_Texture.h
@@ -0,0 +1,69 @@
+#ifndef __IMAGE_TEXTURE_H__
+#define __IMAGE_TEXTURE_H__
+
+#include "Scenes.h"
+
+#include "../Textures/Textures.h"
+
+inline Scene Image_Texture(void)
+{
+    Scene scene;
+
+    // Setup Lights
+    scene.lights.count  = 2;
+    scene.lights.lights = (LightArray *)malloc(sizeof(LightArray) * scene.lights.count);
+
+    scene.lights.lights[0] = (LightArray){.type = LIGHT_POINT, .object.pointLight = {.location = {3.0, -10.0, -5.0}, .colour = {1.0, 1.0, 1.0}, .intensity = 1.0}};
+    scene.lights.lights[1] = (LightArray){.type = LIGHT_POINT, .object.pointLight = {.location = {0.0, -10.0, -5.0}, .colour = {1.0, 1.0, 1.0}, .intensity = 1.0}};
+
+    // Setup Camera
+    scene.cam = (Camera_t){
+        .position        = {2.0, -5.0, -2.0},
+        .lookat          = {0.0, 0.0, 0.0},
+        .up              = {0.0, 0.0, 1.0},
+        .length          = 1.0,
+        .horizontal_size = 1.0,
+        .aspect_ratio    = 1.0, // 800x800
+    };
+    Camera_Update_Geometry(&scene.cam);
+
+    // Textures
+    Texture floor_texture = Texture_Checker_Init((vec2){0.0, 0.0}, 0.0, (vec2){16.0, 16.0}, (vec4){0.0, 0.0, 0.0, 1.0}, (vec4){0.2, 0.2, 0.2, 1.0});
+    Texture image_texture = Texture_Image_Init("../../res/images/crate.png", (vec2){0.0, 0.0}, 0.0, (vec2){1.0, 1.0});
+
+    // Materials
+    scene.mats_count      = 6;
+    scene.mats            = (Material *)malloc(sizeof(Material) * scene.mats_count);
+    scene.mats[0]         = (Material){.base_colour = {0.0, 1.0, 0.0}, .reflectivity = 0.25, .shininess = 0.0, .has_texture = false}; // Floor
+    scene.mats[1]         = (Material){.base_colour = {0.0, 0.0, 1.0}, .reflectivity = 0.0, .shininess = 0.0, .has_texture = true};   // Image Material
+    scene.mats[1].texture = image_texture;
+    scene.mats[2]         = (Material){.base_colour = {1.0, 0.2, 0.2}, .reflectivity = 0.8, .shininess = 32.0, .has_texture = false}; // Sphere Material 1
+    scene.mats[3]         = (Material){.base_colour = {0.2, 1.0, 0.2}, .reflectivity = 0.8, .shininess = 32.0, .has_texture = false}; // Sphere Material 2
+    scene.mats[4]         = (Material){.base_colour = {0.2, 0.2, 1.0}, .reflectivity = 0.8, .shininess = 32.0, .has_texture = false}; // Sphere Material 3
+
+    // Setup Objects (Spheres, Planes)
+    scene.objects = Objects_Init(7);
+
+    scene.objects.shapes[0] = (Shape){.type      = SHAPE_PLANE,    // Floor
+                                      .mat       = &scene.mats[0], // Floor
+                                      .transform = Transform_Set((vec3){0.0, 0.0, 1.0}, (vec3){0.0, 0.0, 0.0}, (vec3){16.0, 16.0, 16.0})};
+
+    scene.objects.shapes[1] = (Shape){.type      = SHAPE_PLANE,    // Image Plane
+                                      .mat       = &scene.mats[1], // Image Material
+                                      .transform = Transform_Set((vec3){1.0, -1.0, 0.5}, (vec3){0.0, 0.0, 0.0}, (vec3){0.5, 0.5, 0.5})};
+
+    scene.objects.shapes[2] = (Shape){.type      = SHAPE_SPHERE,   // Sphere
+                                      .mat       = &scene.mats[2], // Sphere Material 1
+                                      .transform = Transform_Set((vec3){-2.0, -2.0, 0.25}, (vec3){0.0, 0.0, 0.0}, (vec3){0.75, 0.75, 0.75})};
+
+    scene.objects.shapes[3] = (Shape){.type      = SHAPE_PLANE,    // Sphere
+                                      .mat       = &scene.mats[3], // Sphere Material 2
+                                      .transform = Transform_Set((vec3){-2.0, -0.5, 0.25}, (vec3){0.0, 0.0, 0.0}, (vec3){0.75, 0.75, 0.75})};
+
+    scene.objects.shapes[4] = (Shape){.type      = SHAPE_PLANE,    // Sphere
+                                      .mat       = &scene.mats[4], // Sphere Material 3
+                                      .transform = Transform_Set((vec3){-2.0, -1.25, -1.0}, (vec3){0.0, 0.0, 0.0}, (vec3){0.75, 0.75, 0.75})};
+
+    return scene;
+}
+#endif // __IMAGE_TEXTURE_H__
