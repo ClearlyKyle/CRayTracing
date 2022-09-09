d8cbb49b21003b1cf2843094fd2e663991e0c453
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-09-09 21:17:29 +0100

    Material_Compute_Colour with function pointers for different materials Compute_Colour

--- src/Materials/Material.h
+++ src/Materials/Material.h
@@ -6,6 +6,7 @@
 #include "../Ray.h"
 #include "../Textures/Textures.h"
 
+// Forward decleratoins
 typedef struct Objects Objects;
 typedef struct Lights  Lights;
 
@@ -15,20 +16,6 @@ enum Material_Type
     MATERIAL_SIMPLE,
     MATERIAL_REFRACTIVE
 };
-
-// typedef struct SimpleMaterial
-//{
-//     vec3   base_colour;
-//     double reflectivity;
-//     double shininess;
-// } SimpleMaterial;
-
-// typedef struct Refractive
-//{
-//     vec3   base_colour;
-//     double reflectivity;
-
-//} Refractive;
 
 typedef struct Material
 {
@@ -40,46 +27,17 @@ typedef struct Material
     double translucency;
     double ior;
 
-    // union
-    //{
-    //     struct refractive
-    //     {
-    //     };
-    // };
-
     Texture texture;
     bool    has_texture;
 } Material;
 
-// -----------------------------------------------------------------------
+// ----------------------------------------------------------------------
+static const int Max_Reflection_Rays  = 3;
+static int       Reflection_Ray_Count = 0;
+// void             Material_Set_Reflection_Ray_Count(const int value);
 
-void Material_Set_Reflection_Ray_Count(const int value);
-
-// Function to return the color of the material.
-// vec3 Material_Compute_Colour(Objects      objects,
-//                             const Lights lights,
-//                             const size_t current_object_index,
-//                             vec3 *const  int_point,
-//                             vec3 *const  local_normal,
-//                             const Ray   *camera_ray);
-
-// Function to compute diffuse color.
-vec3 Material_Compute_Diffuse_Colour(Objects      objects,
-                                     const Lights lights,
-                                     const size_t current_object_index,
-                                     vec3 *const  int_point,
-                                     vec3 *const  local_normal,
-                                     const vec3   base_colour);
-
-// Function to compute the reflection color.
-vec3 Material_Compute_Reflection_Colour(const Material mat,
-                                        Objects        objects,
-                                        const Lights   lights,
-                                        const size_t   current_object_index,
-                                        vec3 *const    int_point,
-                                        vec3 *const    local_normal,
-                                        const Ray     *incident_ray,
-                                        const vec3     base_colour);
+// Compute Colour Function pointer
+typedef vec3 (*Compute_Colour_fptr)(Objects objects, const Lights lights, const size_t current_object_index, vec3 *const int_point, vec3 *const local_normal, const Ray *camera_ray);
 
 // Function to cast a ray into the scene.
 bool Material_Cast_Ray(const Ray     cast_ray,
@@ -90,61 +48,30 @@ bool Material_Cast_Ray(const Ray     cast_ray,
                        vec3 *const   closests_local_normal,
                        vec3 *const   closests_local_colour);
 
-// Function to return the color.
-vec3 Simple_Material_Compute_Colour(const Material mat,
-                                    Objects        objects,
-                                    const Lights   lights,
-                                    const size_t   current_object_index,
-                                    vec3 *const    int_point,
-                                    vec3 *const    local_normal,
-                                    const Ray     *camera_ray);
-
-// Function to compute specular highlights.
-vec3 SimpleMaterial_Compute_Specular(const Material mat,
-                                     Objects        objects,
-                                     const Lights   lights,
-                                     vec3 *const    int_point,
-                                     vec3 *const    local_normal,
-                                     const Ray     *camera_ray);
-
-vec3 Refrective_Compute_Colour(const Material mat,
-                               Objects        objects,
-                               const Lights   lights,
-                               const size_t   current_object_index,
-                               vec3 *const    int_point,
-                               vec3 *const    local_normal,
-                               const Ray     *camera_ray);
-
-vec3 Refractive_Material_Compute_Translucency(Objects      objects,
-                                              const Lights lights,
-                                              const size_t current_object_index,
-                                              vec3 *const  int_point,
-                                              vec3 *const  local_normal,
-                                              const Ray   *camera_ray);
-
-vec3 Refractive_Material_Compute_Specular(const Material mat,
-                                          Objects        objects,
-                                          const Lights   lights,
-                                          vec3 *const    int_point,
-                                          vec3 *const    local_normal,
-                                          const Ray     *camera_ray);
-
 // Compute Colour --------------------------------------------------------
-
-// Function pointer
-typedef vec3 (*Compute_Colour_fptr)(const Material mat,
-                                    Objects        objects,
-                                    const Lights   lights,
-                                    const size_t   current_object_index,
-                                    vec3 *const    int_point,
-                                    vec3 *const    local_normal,
-                                    const Ray     *camera_ray);
-
+// Function to return the color of the material.
 vec3 Material_Compute_Colour(Objects      objects,
                              const Lights lights,
                              const size_t current_object_index,
                              vec3 *const  int_point,
                              vec3 *const  local_normal,
                              const Ray   *camera_ray);
+// BASE MATERIAL
+vec3 Material_Base_Compute_Colour(Objects objects, const Lights lights, const size_t current_object_index, vec3 *const int_point, vec3 *const local_normal, const Ray *camera_ray);
+vec3 Material_Base_Compute_Diffuse_Colour(Objects objects, const Lights lights, const size_t current_object_index, vec3 *const int_point, vec3 *const local_normal, const vec3 base_colour);
+vec3 Material_Base_Compute_Reflection_Colour(Objects objects, const Lights lights, const size_t current_object_index, vec3 *const int_point, vec3 *const local_normal, const Ray *incident_ray, const vec3 base_colour);
+
+// SIMPLE MATERIAL
+vec3 Material_Simple_Compute_Colour(Objects objects, const Lights lights, const size_t current_object_index, vec3 *const int_point, vec3 *const local_normal, const Ray *camera_ray);
+
+// REFRACTIVE MATERIAL
+vec3 Material_Refrective_Compute_Colour(Objects objects, const Lights lights, const size_t current_object_index, vec3 *const int_point, vec3 *const local_normal, const Ray *camera_ray);
+
+// List of Compute Colour Functions
+const static Compute_Colour_fptr Compute_Colour_fptr_array[] = {
+    Material_Base_Compute_Colour,
+    Material_Simple_Compute_Colour,
+    Material_Refrective_Compute_Colour,
+};
 
 #endif // __MATERIAL_H__
